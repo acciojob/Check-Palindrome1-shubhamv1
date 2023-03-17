2db48b1b68105782869c685f313d3ad4a6293c22
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-03-17 10:15:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-// complete the given function
-
-function palindrome(str){
-
+// function to check if a string is a palindrome
+function palindrome(str) {
+  // remove non-alphanumeric characters and convert to lowercase
+  str = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
+  // reverse the string and compare with original
+  return str === str.split('').reverse().join('');
 }
-module.exports = palindrome
+
+// test the function with some sample inputs
+console.log(palindrome("A man, a plan, a canal: Panama")); // true
+console.log(palindrome("race a car")); // false
+console.log(palindrome("Was it a car or a cat I saw?")); // true
+console.log(palindrome("No 'x' in Nixon")); // true
+
+// display the result on the HTML page
+document.getElementById("result").innerHTML = palindrome("A man, a plan, a canal: Panama");
